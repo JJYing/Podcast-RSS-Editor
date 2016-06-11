0abc0ae1b3ec60543ac0cdfca9f0a12d7c327630
JJ Ying <yingjunjiu@gmail.com>
2016-06-12 02:36:14 +0800

    Dashboard Page updated
    + Added weekday analytics
    = Updated font settings
    = Updated example RSS file

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 
 //Load the XML file
 $xmlDoc = simplexml_load_file('rss.xml','my_node');
+
 //Process episodes
 $t = 0;
 $totalShownoteLinks = 0;
@@ -111,12 +112,71 @@ if ($ep >= 0) {
 elseif ($ep == -2) {
 
 	//Show Dashboard
+	
 	$fileSize = 	number_format((filesize('rss.xml') / 1024),1);
-	$showDashboard = "dashboard-show";
+	$showDashboard = "panel-show";
 	$fullDuration = strtotime($thisDate[0]) - strtotime($thisDate[($t-1)]);
 	$averageCycle = number_format(($fullDuration / $t / 86400),1);
 	$totalYears = number_format(($fullDuration / 31536000),1);
+	$totalSunNo = $totalMonNo = $totalTueNo = $totalWedNo = $totalThuNo = $totalFriNo =$totalSatNo = 0;
+	for ($i = 0; $i < $t; $i++) {
+		$thisDateD[$i] = date('D', strtotime($thisDate2[$i]));
+		switch ($thisDateD[$i])
+		{
+		case 'Sun': $totalSunNo += 1; break;
+		case 'Mon': $totalMonNo += 1; break;
+		case 'Tue': $totalTueNo += 1; break;
+		case 'Wed': $totalWedNo += 1; break;
+		case 'Thu': $totalThuNo += 1; break;
+		case 'Fri': $totalFriNo += 1; break;
+		case 'Sat': $totalSatNo += 1; break;
+		default:
+		  $totalSunNo += 1;
+		}
+	}
+	$totalWeekdayNo = array($totalSunNo, $totalMonNo, $totalTueNo, $totalWedNo, $totalThuNo, $totalFriNo, $totalSatNo);
+	$maxWeekdayNo = max($totalWeekdayNo);
+	$barHeight = 140;
+	$w1 = 45;
+	$j = 0;
+	foreach ($totalWeekdayNo as $value) {
+		$weekNoHeight[$j] = $value / $maxWeekdayNo * $barHeight;
+		$weekBarY[$j] = $barHeight - $weekNoHeight[$j];
+		if ($value == $maxWeekdayNo) {
+			$highestBar[$j] = "highest";
+		}
+		$j ++;
+	}
+	$weekDay = "
+		<ul>
+			<li><svg xmlns='http://www.w3.org/2000/svg' width='$w1' height='$barHeight' viewBox='0 0 $w1 $barHeight' class='$highestBar[0]'>
+			<rect class='chart-bar-1' x='0' y='$weekBarY[0]' width='$w1' height='$weekNoHeight[0]'/></svg><label>Sun</label></li>
+			
+			<li><svg xmlns='http://www.w3.org/2000/svg' width='$w1' height='$barHeight' viewBox='0 0 $w1 $barHeight' class='$highestBar[1]'>
+			<rect class='chart-bar-1' x='0' y='$weekBarY[1]' width='$w1' height='$weekNoHeight[1]'/></svg><label>Mon</label></li>
+			
+			<li><svg xmlns='http://www.w3.org/2000/svg' width='$w1' height='$barHeight' viewBox='0 0 $w1 $barHeight' class='$highestBar[2]'>
+			<rect class='chart-bar-1' x='0' y='$weekBarY[2]' width='$w1' height='$weekNoHeight[2]'/></svg><label>Tue</label></li>
+			
+			<li><svg xmlns='http://www.w3.org/2000/svg' width='$w1' height='$barHeight' viewBox='0 0 $w1 $barHeight' class='$highestBar[3]'>
+			<rect class='chart-bar-1' x='0' y='$weekBarY[3]' width='$w1' height='$weekNoHeight[3]'/></svg><label>Wed</label></li>
+			
+			<li><svg xmlns='http://www.w3.org/2000/svg' width='$w1' height='$barHeight' viewBox='0 0 $w1 $barHeight' class='$highestBar[4]'>
+			<rect class='chart-bar-1' x='0' y='$weekBarY[4]' width='$w1' height='$weekNoHeight[4]'/></svg><label>Thu</label></li>
+			
+			<li><svg xmlns='http://www.w3.org/2000/svg' width='$w1' height='$barHeight' viewBox='0 0 $w1 $barHeight' class='$highestBar[5]'>
+			<rect class='chart-bar-1' x='0' y='$weekBarY[5]' width='$w1' height='$weekNoHeight[5]'/></svg><label>Fri</label></li>
+			
+			<li><svg xmlns='http://www.w3.org/2000/svg' width='$w1' height='$barHeight' viewBox='0 0 $w1 $barHeight' class='$highestBar[6]'>
+			<rect class='chart-bar-1' x='0' y='$weekBarY[6]' width='$w1' height='$weekNoHeight[6]'/></svg><label>Sat</label></li>
+		</ul>
+	";
 	$navHighlight2 = "highlight";
+}
+elseif ($ep == -3) {
+	//Show Settings
+	$showSettings = "panel-show";
+	$navHighlight3 = "highlight";
 }
 else {
 	$panelTitle = "Add New Episode";
@@ -177,6 +237,7 @@ class my_node extends SimpleXMLElement
     }
 }
 ?>
+
 <nav>
 	<a href="?ep=-1" class="logo"><strong>Podcast</strong> RSS Editor</a>
 		<ul>
@@ -186,12 +247,13 @@ class my_node extends SimpleXMLElement
 			<li><a href="?ep=-2" class="<?php echo($navHighlight2);?>"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60">
 			    <path id="Dashboard-2" data-name="Dashboard" class="cls-1" d="M190,54a1,1,0,0,1-1-1V44a1,1,0,0,1,2,0v9A1,1,0,0,1,190,54Zm-8,0a1,1,0,0,1-.914-1.406l4-9a1,1,0,0,1,1.828.813l-4,9A1,1,0,0,1,182,54Zm16,0a1,1,0,0,1-.915-0.594l-4-9a1,1,0,0,1,1.828-.812l4,9A1,1,0,0,1,198,54Zm14-12H168a1,1,0,0,1-1-1V11a1,1,0,0,1,1-1h17a1,1,0,0,1,0,2H169V40h42V12H195a1,1,0,0,1,0-2h17a1,1,0,0,1,1,1V41A1,1,0,0,1,212,42ZM195,16H185a1,1,0,0,1-1-1V7a1,1,0,0,1,1-1h10a1,1,0,0,1,1,1v8A1,1,0,0,1,195,16Zm-9-2h8V8h-8v6Z" transform="translate(-160)"/>
 			</svg>Dashboard</a></li>
-			<li><a href="?ep=-1"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60">
+			<li><a href="?ep=-3" class="<?php echo($navHighlight3);?>"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60">
 			    <path id="settings-2" data-name="settings" class="cls-1" d="M351.331,53h-2.662a3,3,0,0,1-2.952-2.463l-0.385-2.123a18.784,18.784,0,0,1-5.044-2.1l-1.781,1.233a3.06,3.06,0,0,1-3.829-.346L332.8,45.322a3,3,0,0,1-.345-3.829l1.233-1.78a18.808,18.808,0,0,1-2.1-5.045l-2.123-.385A3,3,0,0,1,327,31.331V28.669a3,3,0,0,1,2.463-2.951l2.123-.386a18.808,18.808,0,0,1,2.1-5.045l-1.233-1.78a3,3,0,0,1,.345-3.829l1.883-1.882a3.059,3.059,0,0,1,3.828-.346l1.782,1.233a18.794,18.794,0,0,1,5.044-2.1l0.385-2.122A3,3,0,0,1,348.669,7h2.662a3,3,0,0,1,2.952,2.463l0.385,2.123a18.794,18.794,0,0,1,5.044,2.1l1.781-1.233a3.06,3.06,0,0,1,3.829.346l1.883,1.882a3,3,0,0,1,.345,3.829l-1.233,1.78a18.808,18.808,0,0,1,2.1,5.045l2.123,0.385A3,3,0,0,1,373,28.669v2.662a3,3,0,0,1-2.463,2.951l-2.123.386a18.808,18.808,0,0,1-2.1,5.045l1.233,1.78a3,3,0,0,1-.345,3.829L365.322,47.2a2.981,2.981,0,0,1-2.122.879h0a2.986,2.986,0,0,1-1.706-.533l-1.782-1.233a18.784,18.784,0,0,1-5.044,2.1l-0.385,2.122A3,3,0,0,1,351.331,53Zm-11.073-8.879a0.993,0.993,0,0,1,.542.16,16.856,16.856,0,0,0,5.609,2.331,1,1,0,0,1,.773.8l0.5,2.767a1,1,0,0,0,.984.821h2.662a1,1,0,0,0,.984-0.822l0.5-2.767a1,1,0,0,1,.773-0.8,16.856,16.856,0,0,0,5.609-2.331,1,1,0,0,1,1.112.017l2.32,1.606a1.02,1.02,0,0,0,1.276-.115l1.883-1.882a1,1,0,0,0,.114-1.276l-1.606-2.32a1,1,0,0,1-.018-1.111,16.859,16.859,0,0,0,2.331-5.608,1,1,0,0,1,.8-0.774l2.768-.5A1,1,0,0,0,371,31.331V28.669a1,1,0,0,0-.821-0.984l-2.768-.5a1,1,0,0,1-.8-0.774,16.858,16.858,0,0,0-2.331-5.608,1,1,0,0,1,.018-1.111l1.606-2.32a1,1,0,0,0-.114-1.276l-1.883-1.882a1.02,1.02,0,0,0-1.277-.115L360.312,15.7a1,1,0,0,1-1.112.018,16.862,16.862,0,0,0-5.609-2.331,1,1,0,0,1-.773-0.8l-0.5-2.768A1,1,0,0,0,351.331,9h-2.662a1,1,0,0,0-.984.822l-0.5,2.767a1,1,0,0,1-.773.8,16.862,16.862,0,0,0-5.609,2.331,1,1,0,0,1-1.112-.018l-2.32-1.606a1.02,1.02,0,0,0-1.276.115l-1.883,1.882a1,1,0,0,0-.114,1.276l1.606,2.32a1,1,0,0,1,.018,1.111,16.858,16.858,0,0,0-2.331,5.608,1,1,0,0,1-.8.774l-2.768.5a1,1,0,0,0-.821.983v2.662a1,1,0,0,0,.821.984l2.768,0.5a1,1,0,0,1,.8.774,16.859,16.859,0,0,0,2.331,5.608,1,1,0,0,1-.018,1.111l-1.606,2.32a1,1,0,0,0,.114,1.276l1.883,1.882a1.023,1.023,0,0,0,1.277.115l2.319-1.606A1,1,0,0,1,340.258,44.121ZM350,39a9,9,0,1,1,9-9A9.01,9.01,0,0,1,350,39Zm0-16a7,7,0,1,0,7,7A7.008,7.008,0,0,0,350,23Z" transform="translate(-320)"/>
 			</svg>Channel Settings</a></li>
 		</ul>
 	<section class="nav-dashboard"><?php echo($navDashboard);?></section>
 </nav>
+
 <article class="panel edit-panel">	
 	<h2 class="panel-title right-in-1"><?php echo $panelTitle ?></h2>
 	<form action="index.php?ep=<?php echo $ep?>" method="post">
@@ -229,6 +291,7 @@ class my_node extends SimpleXMLElement
 	<a href='?ep=-1' class="item add-new-item"><h1>Add New Episode</h1></a>
 	<?php echo($itemList);?>
 </aside>
+
 <article class="dashboard panel-in <?php echo($showDashboard);?>">
 	<section class="last-udpate"><strong class="scale-in-1"><?php echo($sinceLastUpdate);?></strong><br />Days Since Last Ep.</section>
 	<section><strong class="scale-in-2"><?php echo($t);?></strong><br />Total Episodes</section>
@@ -238,13 +301,14 @@ class my_node extends SimpleXMLElement
 	
 	<section class="d-average-cycle"><strong class="scale-in-2"><?php echo($averageCycle);?></strong><br />Average Update Cycle</section>
 	<section><strong class="scale-in-3"><?php echo($totalYears);?></strong><br />Years after first Episode</section>
-	<section class="total-hours"><strong class="scale-in-4"><?php echo($totalHours);?></strong><br />Total Hours</section>
 	<section class="d-total-links"><strong class="scale-in-5"><?php echo($totalShownoteLinks);?></strong><br />Links in Shownotes</section>
-	<section class="d-file-size"><strong class="scale-in-5"><?php echo($fileSize);?></strong><br />KB for RSS file</section>
+	<section class="d-weekday"><div class="scale-in-4"><?php echo($weekDay);?></div></section>
+
 </article>
-<script type="text/javascript">
-	
-</script>
+
+<article class="settings panel-in <?php echo($showSettings);?>">
+	<span>Nothing to Show....<br />for now...</span>
+</article>
 
 </body>
 </html>
